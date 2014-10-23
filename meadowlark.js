5febaa6dc73b7fe3bb2d75a0f991639348c4a59a
Ajay R <ajayr@formbuddy.com>
2014-10-23 08:27:07 -0400

    Testing file upload

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var fortune = require('./lib/fortune.js');
+var formidable = require('formidable');
 
 var app = express();
 
@@ -41,6 +42,25 @@ app.get('/tours/oregon-coast', function(req, res){
 
 app.get('/tours/request-group-rate', function(req, res){
     res.render('tours/request-group-rate');
+});
+
+app.get('/contest/vacation-photo',function(req,res){
+    var now = new Date();
+    res.render('contest/vacation-photo',{
+        year: now.getFullYear(),month: now.getMonth()
+    });
+});
+
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files){
+	if(err) return res.redirect(303, '/error');
+	console.log('received fields:');
+	console.log(fields);
+	console.log('received files:');
+	console.log(files);
+	res.redirect(303, '/thank-you');
+    });
 });
 
 //custom 404 error page
