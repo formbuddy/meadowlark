d9c3bab4aafd8ddfa5be9e186b3d4b0ac19b7bb6
Ajay R <ajayr@formbuddy.com>
2014-10-24 10:27:36 -0400

    POST upload

--- meadowlark.js
+++ meadowlark.js
@@ -2,6 +2,7 @@ var express = require('express');
 var fortune = require('./lib/fortune.js');
 var formidable = require('formidable');
 
+
 var app = express();
 
 //setup handlebars and view engine
@@ -13,6 +14,7 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
+app.use(require('body-parser')());
 
 app.use(function(req, res, next){
     res.locals.showTests = app.get('env') !== 'production' &&
@@ -61,6 +63,20 @@ app.post('/contest/vacation-photo/:year/:month', function(req, res){
 	console.log(files);
 	res.redirect(303, '/thank-you');
     });
+});
+
+app.get('/newsletter', function(req, res){
+    // we will learn about CSRF later...for now, we just
+    // provide a dummy value
+    res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+
+app.post('/process', function(req, res){
+    console.log('Form (from querystring): ' + req.query.form);
+    console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+    console.log('Name (from visible form field): ' + req.body.name);
+    console.log('Email (from visible form field): ' + req.body.email);
+    res.redirect(303, '/thank-you');
 });
 
 //custom 404 error page
